9f512d859919e6930976289d3a1b6d914f04c98a
Matt Crook <mattlcrook@icloud.com>
2020-09-27 10:54:52 +1300

    find shortcut implementation

--- src/commands/Find-Shortcut.ps1
+++ src/commands/Find-Shortcut.ps1
@@ -8,4 +8,12 @@ function Find-Shortcut {
         [string]
         $Target
     )
+
+    $shortcuts = Get-ChildItem -Path $Path -Filter *.lnk
+    if (-not $shortcuts) {
+        return $null
+    }
+
+    $shell = New-Object -ComObject WScript.Shell
+    return $shortcuts | Where-Object { $shell.CreateShortcut($_.FullName).TargetPath -eq $Target }
 }
