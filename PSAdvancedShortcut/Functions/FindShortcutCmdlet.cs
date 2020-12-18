8ab2b3b21e14d3700b1068b1b14f36ca77f4df5a
Matt Crook <mattlcrook@icloud.com>
2020-12-19 09:02:00 +1300

    implement more verbose logging, allow pipelining for the find shortcut cmdlet

--- PSAdvancedShortcut/Functions/FindShortcutCmdlet.cs
+++ PSAdvancedShortcut/Functions/FindShortcutCmdlet.cs
@@ -9,33 +9,44 @@ namespace PSAdvancedShortcut.Functions
     public class FindShortcutCmdlet : PSCmdlet
     {
         [Parameter(
-            Mandatory = true,
-            HelpMessage = "Path to look for shortcuts")]
+            Mandatory = false,
+            HelpMessage = "Path to look for shortcuts, defaults to current directory")]
         public string Path { get; set; }
 
         [Parameter(
             Mandatory = true,
+            ValueFromPipeline = true,
             HelpMessage = "Target to match within the shortcuts")]
         public string Target { get; set; }
 
         [Parameter(
             Mandatory = false,
             HelpMessage = "Recursively search into subdirectories, including network drives and symbolic links")]
-        public bool Recurse { get; set; }
+        public SwitchParameter Recurse { get; set; }
 
         protected override void ProcessRecord()
         {
+            if (string.IsNullOrEmpty(Path))
+            {
+                Path = SessionState.Path.CurrentFileSystemLocation.Path;
+                WriteVerbose("Path parameter was not set, defaulting to current PS session filesystem directory");
+            }
+
+            WriteVerbose($"Looking for shortcut files in the path: {Path}");
             var files = Directory.GetFiles(Path, "*.lnk", Recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
             if (files.Length == 0)
             {
+                WriteVerbose("Could not find any shortcut files in the specified path");
                 WriteObject(null);
                 return;
             }
 
+            WriteVerbose($"Checking {files.Count()} shortcut files for the target: {Target}");
             var shell = new WshShell();
             var matches = files
                 .Where(file => ((IWshShortcut)shell.CreateShortcut(file)).TargetPath == Target)
                 .ToArray();
+            WriteVerbose($"Found {matches.Count()} shortcut matches");
             WriteObject(matches);
         }
     }
